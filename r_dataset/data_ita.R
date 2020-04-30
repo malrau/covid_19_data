bec053741255fec8e7dd7aee1cdfb6fe1069f300
malrau <maurizio.larosa@unime.it>
2020-04-30 23:38:22 +0200

    Update .csv data with those released by the Protezione Civile on April, 30. Update 'r_dataset/data_ita.R'with dates of subsequent DPCM. Update tables, plots and web page

--- r_dataset/data_ita.R
+++ r_dataset/data_ita.R
@@ -21,10 +21,21 @@ names(covid_reg)
 covid_data <- rbind(covid_ita,covid_reg) #Here I merge the two datasets
 covid_data$mortalità <- covid_data$deceduti/covid_data$totale_casi #Mortality rate
 covid_data$nuovi_tamponi <- covid_data$nuovi_positivi/covid_data$tamponi #Capacity of tampoons to signal the increase of contagions
-covid_data$mado <- as.numeric(italia$date=="2020-03-07") #People from Madonna di Casmpiglio to Messina
-covid_data$esodo1 <- as.numeric(italia$date=="2020-03-08") #First coming back of people from Northern Italy to the South (following the DPCM rendering Lombardia and fourteen provinces from Piemonte, Veneto, Emilia Romagna and Marche "Zona rossa")
-covid_data$esodo2 <- as.numeric(italia$date=="2020-03-13") #Second coming back of people from Northern Italy to the South (following the DPCM rendering the whole country "Zona rossa" and shutting down economic activities)
-covid_data$esodo3 <- as.numeric(italia$date=="2020-03-22") #Third coming back of people from Northern Italy to the South (following the Ordinanza dei Ministeri della Salute e dell'Interno forbidding movements between Comuni and the DPCM shutting down further economic activities. Note that, as for Sicily, the number of people moving to the South has been lower by 70% than in previous exoduses)
+covid_data$decreto_legge <- as.numeric(covid_data$date=="2020-02-23") #D.L. 6/2020 (2020/02/23), converted to L. 13/2020 (2020/03/05)
+covid_data$dpcm1 <- as.numeric(covid_data$date=="2020-02-23") #Measures of containment of COVID-19 contagions (DPCM 2020/02/23)
+covid_data$dpcm2 <- as.numeric(covid_data$date=="2020-02-25") #Further measures of containment of COVID-19 contagions (DPCM 2020/02/25)
+covid_data$dpcm3 <- as.numeric(covid_data$date=="2020-03-01") #Further measures of containment of COVID-19 contagions (DPCM 2020/03/01)
+covid_data$dpcm4 <- as.numeric(covid_data$date=="2020-03-04") #Further measures of containment of COVID-19 contagions (DPCM 2020/03/04)
+covid_data$dpcm5 <- as.numeric(covid_data$date=="2020-03-08") #Further measures of containment of COVID-19 contagions - Movement and school lockdown of Lombardia + 14 provinces (DPCM 2020/03/08)
+covid_data$dpcm6 <- as.numeric(covid_data$date=="2020-03-09") #Further measures of containment of COVID-19 contagions - Movement and school lockdown of Italy (DPCM 2020/03/09)
+covid_data$dpcm7 <- as.numeric(covid_data$date=="2020-03-11") #Further measures of containment of COVID-19 contagions - Economic lockdown of Italy (DPCM 2020/03/11)
+covid_data$dpcm8 <- as.numeric(covid_data$date=="2020-03-22") #Further measures of containment of COVID-19 contagions (DPCM 2020/03/22)
+covid_data$dpcm9 <- as.numeric(covid_data$date=="2020-04-26") #First release of the lockdown from 2020/05/04 (DPCM 2020/04/26)
+covid_data$pre_esodo <- as.numeric(covid_data$date=="2020-02-23") #Article of "La Repubblica" ("Coronavirus, l'illusione della grande fuga da Milano. Ecco i veri numeri degli spostamenti verso sud" April, 23) reports that the exodus from Milan to the South of Italy has happened on February, 23 rather than during March, as feared
+covid_data$mado <- as.numeric(covid_data$date=="2020-03-07") #People from Madonna di Casmpiglio to Messina
+covid_data$esodo1 <- as.numeric(covid_data$date=="2020-03-08") #First coming back of people from Northern Italy to the South (following the DPCM rendering Lombardia and fourteen provinces from Piemonte, Veneto, Emilia Romagna and Marche "Zona rossa")
+covid_data$esodo2 <- as.numeric(covid_data$date=="2020-03-13") #Second coming back of people from Northern Italy to the South (following the DPCM rendering the whole country "Zona rossa" and shutting down economic activities)
+covid_data$esodo3 <- as.numeric(covid_data$date=="2020-03-22") #Third coming back of people from Northern Italy to the South (following the Ordinanza dei Ministeri della Salute e dell'Interno forbidding movements between Comuni and the DPCM shutting down further economic activities. Note that, as for Sicily, the number of people moving to the South has been lower by 70% than in previous exoduses)
 plot(covid_data$date,covid_data$totale_positivi) #General plot (Italy and regions)
 write.csv(covid_data,"covid_data.csv",row.names=F) #I save the dataframe as a .csv file. I added the 'row.names=F' option which became necessary to avoid generating an empty first column in the generated file (that column represents row names)
 ############################
